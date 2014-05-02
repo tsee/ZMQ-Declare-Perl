d80cd9ab773c2968e49373c3162e42f655423115
Steffen Mueller <smueller@cpan.org>
2014-05-02 14:39:25 +0200

    version bump

--- lib/ZMQ/Declare.pm
+++ lib/ZMQ/Declare.pm
@@ -139,7 +139,7 @@ Steffen Mueller E<lt>smueller@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011,2012 by Steffen Mueller
+Copyright (C) 2011,2012,2014 by Steffen Mueller
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.1 or,
